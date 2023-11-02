dfca88fecf84e7682c138e1d5247a721012cc286
H123C456 <1556618318@qq.com>
2023-11-02 15:06:59 +0800

    初始

--- November/src/main/java/com/ruxi/testNove/CountPoints.java
+++ November/src/main/java/com/ruxi/testNove/CountPoints.java
@@ -1,2 +1,35 @@
-package com.ruxi.testNove;public class CountPoints {
+package com.ruxi.testNove;
+
+import java.util.*;
+
+public class CountPoints {
+
+    public static void main(String[] args) {
+        String rings = "B0B6G0R6R0R6G9";
+        String regex = "[0-9]";
+        String regex1 = "[A-Z]";
+
+
+        String[] letter = rings.split(regex);
+        rings.split(regex1)
+        Arrays.stream(letter).forEach(p-> System.out.print(p));
+        System.out.println();
+        Arrays.stream(num).forEach(p-> System.out.print(p));
+
+
+        List<String> testList = new ArrayList<>();
+
+        for (int i=0;i< num.length;i++){
+            if (num[i]==""){
+                continue;
+            }
+            int i1 = Integer.parseInt(num[i]);
+            System.out.println(i1);
+
+        }
+
+
+
+    }
 }
+
--- November/src/main/java/com/ruxi/testNove/TestMap.java
+++ November/src/main/java/com/ruxi/testNove/TestMap.java
@@ -1,2 +1,13 @@
-package com.ruxi.testNove;public class TestMap {
+package com.ruxi.testNove;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMap {
+    public static void main(String[] args) {
+
+        Map<Integer,String> testMap = new HashMap<>();
+
+
+    }
 }
