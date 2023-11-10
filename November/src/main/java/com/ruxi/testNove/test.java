2eb87f499c576557fee25bde3812cb65fc27fcc6
H123C456 <1556618318@qq.com>
2023-11-10 18:59:47 +0800

    今日11.10

--- November/src/main/java/com/ruxi/testNove/test.java
+++ November/src/main/java/com/ruxi/testNove/test.java
@@ -1,10 +1,14 @@
 package com.ruxi.testNove;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class test {
 
     public static void main(String[] args) {
-        String s = "01000111";
-
-
+        int[] spells = {5,1,3};
+        spells[2]=4;
+        Arrays.stream(spells).forEach(p-> System.out.println(p));
+        System.out.println(spells.length);
     }
 }
